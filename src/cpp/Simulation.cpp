edadfae1a01634652edf68c4e686d3069f538f1b
Henne <henne90gen@gmail.com>
2018-08-25 11:49:51 +0200

    Reverting refactoring

--- src/cpp/Simulation.cpp
+++ src/cpp/Simulation.cpp
@@ -68,3 +68,21 @@ int mainLoop() {
     tearDownGLFW(&window);
     return 0;
 }
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        fprintf(stderr, "Usage: call pythonfile funcname [args]\n");
+        return 1;
+    }
+
+    setupPython();
+    // callPythonFunction(argc, argv);
+
+    int error = mainLoop();
+    if (error) {
+        return error;
+    }
+
+    tearDownPython();
+    return 0;
+}
